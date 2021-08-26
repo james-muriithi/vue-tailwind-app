e45e568b9d62b7505a2f28665a0879663e5388f6
james-muriithi <muriithijames556@gmail.com>
2021-08-26 15:28:49 +0300

    added vue-router

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue';
 // tailwind
 import './assets/tailwind.css'
 
-const app = createApp(App);
+import router from './router'
+
+const app = createApp(App).use(router);
 
 app.mount('#app')
